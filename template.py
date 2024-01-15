a4e69c27e12461b5576ccc618927275f175bd49f
Komal-patra <komalfsds2022@gmail.com>
2024-01-15 20:35:43 +0530

    Modified template.py

--- template.py
+++ template.py
@@ -5,10 +5,9 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "mlProject"
+project_name = "WineQuality"
 
-
-
+# list of files needed to create
 list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
@@ -31,3 +30,22 @@ list_of_files = [
     "templates/index.html"
 
 ]
+
+#using the above list of files, the folders and files are created
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
