b0dd856be3d375e66b653fb3aacf044b090921cd
Komal-patra <komalfsds2022@gmail.com>
2024-01-16 23:41:43 +0530

    Model Trainer Stage coded and executed successfully

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from WineQuality.logging import logger
 from src.WineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.WineQuality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.WineQuality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.WineQuality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -15,6 +16,7 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
 STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -25,6 +27,7 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
 STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -34,3 +37,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
